deaee75baaa3491300a28bd55ea73ca0267d871b
jonaskas <jonaskas-miguelito@hotmail.com>
2018-03-13 16:37:55 +0000

    ficha de trabalho 2 ex2

---
+++ Fichas Praticas/Ficha prática 2/exercicio3.py
@@ -0,0 +1,21 @@
+"""
+Exercicio 3
+"""
+
+primeiro = float(input('Insira o 1º Numero: '))
+segundo = float(input('Insira o 2º Numero: '))
+operacao = input('+ Soma\n/ Divisão\n- Subtração\n* Multiplicação\n')
+
+if operacao == '+':
+    resultado = primeiro + segundo
+elif operacao == '/':
+    resultado = primeiro / segundo
+elif operacao == '-':
+    resultado = primeiro - segundo
+elif operacao == '*':
+    resultado = primeiro * segundo
+else:
+     resultado = "Invalido "
+
+print(resultado) 
+    
