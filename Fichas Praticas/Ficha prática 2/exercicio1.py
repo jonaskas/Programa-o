61d5536592353883c6360eb98303adf3a35ccb02
jonaskas <jonaskas-miguelito@hotmail.com>
2018-03-13 15:45:07 +0000

    ficha de trabalho 2 ex1

--- Fichas Praticas/Ficha prática 2/exercicio1.py
+++ Fichas Praticas/Ficha prática 2/exercicio1.py
@@ -5,6 +5,6 @@ primeiro = int(input('Insira o 1º Numero: '))
 segundo = int(input('Insira o 2º Numero: '))
 
 if primeiro > segundo :
-    print('O Maior numero é:'primeiro)
+    print('O Maior numero é:',primeiro)
 
-print('O Maior numero é:'segundo)
+print('O Maior numero é:',segundo)
