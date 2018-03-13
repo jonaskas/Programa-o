dde10f113297883fe938e71dfb12f7f97949def5
jonaskas <jonaskas-miguelito@hotmail.com>
2018-03-13 15:13:07 +0000

    Conclusao da ficha da avaliação v2

--- Fichas de avaliação/Python_fp_1_aval/exercicio4.py
+++ Fichas de avaliação/Python_fp_1_aval/exercicio4.py
@@ -5,8 +5,8 @@ nota1 = float(input('\nIntroduza a 1ª Nota: '))
 nota2 = float(input('Introduza a 2ª Nota: '))
 nota3 = float(input('Introduza a 3ª Nota: '))
 
-media = float((nota1 * 25 + nota2 * 35 + nota3 * 40)//100)
-media_inteira = int(media)
+media = float((nota1 * 25 + nota2 * 35 + nota3 * 40)/100)
+media_inteira = int((nota1 * 25 + nota2 * 35 + nota3 * 40)//100)
 media_modulo = (nota1 * 25 + nota2 * 35 + nota3 * 40)%100
 
 print('\nMedia = {}\nMedia Inteira = {}\nMedia Modulo = {} \n'.format(media,media_inteira,int(media_modulo)))
