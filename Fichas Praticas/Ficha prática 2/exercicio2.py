65c82f1ca40e7ad084e07854d87143eaa742e24c
jonaskas <jonaskas-miguelito@hotmail.com>
2018-03-13 15:55:00 +0000

    ficha de trabalho 2 ex2

---
+++ Fichas Praticas/Ficha prática 2/exercicio2.py
@@ -0,0 +1,12 @@
+"""
+Exercicio 2
+"""
+primeiro = int(input('Insira o 1º Numero: '))
+segundo = int(input('Insira o 2º Numero: '))
+
+if primeiro > segundo:
+    print(primeiro, 'e', segundo)
+else:
+    print(segundo, 'e', primeiro)
+    
+
