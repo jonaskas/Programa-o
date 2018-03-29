9379839ac81d8479bfc957a48bd0cd4a89039d9d
jonaskas <jonaskas-miguelito@hotmail.com>
2018-03-29 14:43:33 +0100

    exercicio3 da ficha de avaliacao feito

--- Fichas de avaliação/Python_fp_2_aval/exercicio3.py
+++ Fichas de avaliação/Python_fp_2_aval/exercicio3.py
@@ -5,3 +5,7 @@ Exercico 3
 numero = int(input('Introduza um número: '))
 
 resultado = numero % 2
+if resultado == 0 :
+     print("O número {} é par".format(numero))
+else:
+    print("O número {} é ímpar".format(numero))
