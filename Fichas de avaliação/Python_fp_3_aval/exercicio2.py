6f24272a9cdd3cb611474032b277881fefa0aa84
jonaskas <jonaskas-miguelito@hotmail.com>
2018-03-31 19:37:12 +0100

    Exercicio 2 da ficha de trabalho 3 feita

--- Fichas de avaliação/Python_fp_3_aval/exercicio2.py
+++ Fichas de avaliação/Python_fp_3_aval/exercicio2.py
@@ -4,3 +4,18 @@ Exercicio 2
 
 numero = int(input('Introduza um numero para confirmar se é primo ou não: '))
 
+if numero == 2:
+    print('O número {} é primo'.format(numero))
+else:
+    while numero > 1:
+        for i in range(2, numero):
+            if numero % i == 0:
+                print('O número {} não é primo'.format(numero))
+                break
+            else:
+                print('O número {} é primo'.format(numero))
+                break
+        break
+
+if numero <= 1:
+    print('Para ser considerado numero primo tem de ser um numero superior a 1!')   
