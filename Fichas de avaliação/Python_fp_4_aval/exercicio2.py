8b35e7f5f913a5651b6817bd016872ecbc03ff51
jonaskas <jonaskas-miguelito@hotmail.com>
2018-04-10 15:30:04 +0100

    Atualização ex1 da ficha para avaliação nº4

--- Fichas de avaliação/Python_fp_4_aval/exercicio2.py
+++ Fichas de avaliação/Python_fp_4_aval/exercicio2.py
@@ -1,3 +1,21 @@
 """
 Exxercicio 2
 """
+euro = 1.231
+dolar = 0.8089
+
+def convertor_euro(quantidade):
+    return (quantidade * euro) / 1
+
+def convertor_dolar(quantidade):
+    return (quantidade * dolar) / 1
+
+def teste():
+    escolha = input("Introdua em que moeda queres converter €(E) ou $(D): ")
+    quantidade = float(input('Introduza umaa quantidade: '))
+    if escolha == 'E':
+        print ('{}€ sao {}$'.format(quantidade, convertor_euro(quantidade)))
+    elif escolha == 'D':
+        print ('{}$ sao {}€'.format(quantidade, convertor_dolar(quantidade)))
+
+teste()
