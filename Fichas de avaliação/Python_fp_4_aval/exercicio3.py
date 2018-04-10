2aa902bc80140c2195ab5b73f2a0f93d2812a38e
jonaskas <jonaskas-miguelito@hotmail.com>
2018-04-10 16:24:51 +0100

    Atualização

--- Fichas de avaliação/Python_fp_4_aval/exercicio3.py
+++ Fichas de avaliação/Python_fp_4_aval/exercicio3.py
@@ -1,14 +1,21 @@
 """
 Exxercicio 3
 """
-def desenhar(carater,linhas,colunas):
-    
+def figura(linhas, colunas, caracter) :
+     
+    for i in range(1, linhas + 1) :
+        for j in range(1, colunas + 1) :
+            if (i == 1 or i == linhas or j == 1 or j == colunas) :
+                print(caracter, end="")            
+            else :
+                print(" ", end="")            
+        print()
+ 
+ 
+linhas = int(input('Quantas linhas deseja: '))
+colunas = int(input('Quantas colunas deseja: '))
+caracter = input('Escolha o caracter do desenho: ')
 
 
+figura(linhas, colunas, caracter)      
 
-
-
-caracter = input('Introduza um caracter: ')
-linhas = int(input('Introduza o numero de linhas: '))
-colunas = int(input('Introduza o numero de colunas: '))
-desenhar(carater,linhas,colunas)
