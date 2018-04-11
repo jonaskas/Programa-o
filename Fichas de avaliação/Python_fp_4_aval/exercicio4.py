76f4606fec6d675895bd8916126d7a9c77a7c921
jonaskas <jonaskas-miguelito@hotmail.com>
2018-04-11 17:46:46 +0100

    ex 4 feito

--- Fichas de avaliação/Python_fp_4_aval/exercicio4.py
+++ Fichas de avaliação/Python_fp_4_aval/exercicio4.py
@@ -2,3 +2,15 @@
 Exxercicio 4
 """
 
+def imprimir(n):
+    if n == -1:
+        return 0
+    elif imprimir(n -1):
+        return n 
+    print(n)
+
+numero = int(input('Introduza um numero: '))
+
+imprimir(numero)
+
+
