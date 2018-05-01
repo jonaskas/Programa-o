ef84c6629a128c38c7d456607433543a35f5b59c
jonaskas <jonaskas-miguelito@hotmail.com>
2018-05-01 13:33:20 +0100

    Ficha 6 resolvida

--- Fichas de avaliação/Python_fp_6_aval/exercicio2.py
+++ Fichas de avaliação/Python_fp_6_aval/exercicio2.py
@@ -1,12 +1,20 @@
 """
 Exercicio 2
 """
-carater = []
+def  quantidade(carater):  
+    teste = input('Introduza 1 caratere: ')
+    contar = carater.count(teste)
+    print("A letra {} existe: {} vezes.".format(teste, contar)) 
+    for i,j in enumerate(carater):
+        if j == teste:
+            print("A letra {} encontra-se na posição {}.".format(j,i))
 
+carater = []
 x = 1
 while x <= 10:
-    n = input('Digite um carater: [ %s ]: '%x))
-    carater.append(n)
+    c = input('Digite um carater: ')
+    carater.append(c)
     x += 1
 
-teste = input('Introduza 1 caratere: ')
+
+quantidade(carater)
